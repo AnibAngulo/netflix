6768ddbeb4a8f5bdad11f1bb572f33747b572a37
AnibAngulo <117378316+AnibAngulo@users.noreply.github.com>
2022-12-04 13:58:56 -0600

    Add files via upload

--- Netflix.py
+++ Netflix.py
@@ -1,33 +1,38 @@
+#Anibal Jose Angulo Cardoza
+#A01654684
+
 import streamlit as st
 import pandas as pd
+from os.path import splitext
 
 st.title('Streamlit - Netflix')
+st.subheader("Anibal Angulo - A01654684")
 sidebar = st.sidebar
 
 DATA_URL = 'movies.csv'
 
-st.header("Tabla de Peliculas")
 @st.cache
 def load_data():
     data = pd.read_csv(DATA_URL)
     return data
 data = load_data()
-agree = sidebar.checkbox("Quieres ver la tabla de peliculas")
+
+st.header("Dataset")
+agree = sidebar.checkbox("Quieres ver la base de datos ? ")
 if agree:
     st.dataframe(data)
-
 
 @st.cache
 def load_data_byname(name):
     data = pd.read_csv(DATA_URL)
-    filtered_data_byname = data[data['name'].str.contains(name)]
+    filtered_data_byname = data[data['name'].str.upper().str.contains(name)]
 
     return filtered_data_byname
 
 myname = sidebar.text_input('Nombre de la pelicula: ')
 btnname = sidebar.button('Buscar por nombre')
 if (btnname):
-    filterbyname = load_data_byname(myname)
+    filterbyname = load_data_byname(myname.upper())
     count_row = filterbyname.shape[0]
     st.write(f"Peliculas Totales: {count_row}")
 
@@ -40,11 +45,6 @@ def load_data_bydirector(director):
 
     return filtered_data_bydirector
 
-@st.cache
-def load_data():
-    data = pd.read_csv(DATA_URL)
-    return data
-data = load_data()
 director = sidebar.selectbox('Nombre del Director: ',data['director'].unique())
 btndirector = st.sidebar.button('Buscar por Director')
 
